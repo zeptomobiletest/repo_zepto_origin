be76585561decf5edf5382140c7e336c8d75f37f
tiffany law <ykltiffany@gmail.com>
2018-10-05 17:16:51 +0800

    control commit master

--- Documents/GitHub/repo_zepto3/index.php
+++ Documents/GitHub/repo_zepto3/index.php
@@ -8,4 +8,6 @@ echo "<br><br>Finally DONE !!!!!";
 echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server";
 echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server 2";
 
+echo "<br><br>GIT PUSH repo zepto3 control commit master";
+
 ?>
