548046e1830d5e39a03f867edf6924cb89d2669b
tiffany law <ykltiffany@gmail.com>
2018-10-05 17:14:34 +0800

    auto deploy master 2

--- Documents/GitHub/repo_zepto3/index.php
+++ Documents/GitHub/repo_zepto3/index.php
@@ -6,5 +6,6 @@ echo "Welcome World";
 echo "<br><br>Finally DONE !!!!!";
 
 echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server";
+echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server 2";
 
 ?>
