e6a97584d7d0fc63827b840c860d844b18b099ae
tiffany law <ykltiffany@gmail.com>
2018-10-05 16:49:41 +0800

    first commit

---
+++ Documents/GitHub/repo_zepto3/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Welcome World";
+
+
+echo "<br><br>Finally DONE !!!!!";
+
+echo "<br><br>GIT PUSH repo zepto2 auto deploy to live server";
+echo "<br><br>GIT PUSH repo zepto2 control tiffany1845 can push master 1";
+echo "<br><br>GIT PUSH repo zepto2 control tiffany1845 can push master 2 add fork";
+
+?>
